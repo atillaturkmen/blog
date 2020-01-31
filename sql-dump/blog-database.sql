8d9d45b996aacb338174e3d75c47645d2f5f189c
atillaturkmen <turkmenatilla522@gmail.com>
2020-01-31 18:47:45 +0300

    update blog-database.sql

--- sql-dump/blog-database.sql
+++ sql-dump/blog-database.sql
@@ -96,6 +96,35 @@ LOCK TABLES `users` WRITE;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+DROP TABLE IF EXISTS `reviews`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reviews` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `article_id` int(5) NOT NULL,
+  `point` int(1) NOT NULL,
+  `reviewed_at` datetime NOT NULL,
+  `user_ip` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`article_id`)
+    REFERENCES articles(`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `click`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `click` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `article_id` int(5) NOT NULL,
+  `clicked_at` datetime NOT NULL,
+  `user_ip` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`article_id`)
+    REFERENCES articles(`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
